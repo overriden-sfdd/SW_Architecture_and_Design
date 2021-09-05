5100cce48687e58599ce386c05d465f10af0f52e
Ruslan Ershov <russell.ershov@shelfmatch.com>
2021-09-06 00:51:52 +0300

    Small restucture trial #2

---
+++ a1/src/main/java/com/edustart/Main.java
@@ -0,0 +1,13 @@
+package com.edustart;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "=== Welcome to the SW Architecture and Design Course ===" );
+    }
+}
